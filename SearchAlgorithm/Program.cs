25912badab76cd0375dfe28e4be747cb7f3d4c7c
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-10-24 18:27:04 +0800

    membuat method untuk menjalankan program

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -104,5 +104,51 @@ namespace SearchAlgorithm
                 Console.Write("\nContinue search (y\n):");
                 ch = char.Parse(Console.ReadLine());
             } while ((ch == 'y') || (ch == 'Y'));
+        }
+        static void Main(string[] args)
+        {
+            Program myList = new Program();
+            int pilihanmenu;
+            do
+            {
+                Console.WriteLine("Menu Option");
+                Console.WriteLine("===============");
+                Console.WriteLine("1.Linear search");
+                Console.WriteLine("2.Binary search");
+                Console.WriteLine("3.Exit");
+                Console.Write("Enter you choice (1,2,3) : ");
+                pilihanmenu = Convert.ToInt32(Console.ReadLine());
+
+                switch (pilihanmenu)
+                {
+                    case 1:
+                        Console.WriteLine("");
+                        Console.WriteLine("...................");
+                        Console.WriteLine("Linear Search");
+                        Console.WriteLine("...................");
+                        myList.input();
+                        myList.LinearSearch();
+                        break;
+                    case 2:
+                        Console.WriteLine("");
+                        Console.WriteLine("...................");
+                        Console.WriteLine("Binary search");
+                        Console.WriteLine("...................");
+                        myList.input();
+                        myList.BinarySearch();
+                        break;
+                    case 3:
+
+                        Console.WriteLine("Exit. ");
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;
+                }
+                //to exit from the console
+                Console.WriteLine("\n\nPress Return to exit. ");
+                Console.ReadLine();
+            } while (pilihanmenu != 3);
+        }
     }
 }
