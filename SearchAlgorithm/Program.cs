52515f8200c3549dcb40235017c717a40bb2d13c
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-10-24 17:27:20 +0800

    membuat method untuk input elemen array

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -6,5 +6,40 @@ using System.Threading;
 
 namespace SearchAlgorithm
 {
-    
+    class Program
+    {
+        //array to be searched
+        int[] arr = new int[20];
+        //Number of elements to store in the array
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements. \n");
+
+            }
+            //Accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" Enter array elements ");
+            Console.WriteLine("----------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + "> ");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+
+    }
 }
